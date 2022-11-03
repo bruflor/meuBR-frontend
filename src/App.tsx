469612652fcf2c3acbfaa248356e7ff87d4bb52c
Bruna Flor <brn.flor@gmail.com>
2022-11-03 16:16:26 +0000

    categories

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { Col, Container, Row } from "react-bootstrap";
+import "./App.scss";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    <Container className="App d-flex flex-column justify-content-center ">
+      <Row
+        className="gap-2 d-flex justify-content-center py-1 text-uppercase  fw-light"
+        style={{ height: "200px", fontSize: "12px" }}
+      >
+        <Col
+          xs={7}
+          md={8}
+          className="bg-secondary  p-2 d-flex align-items-end p-2"
+        >
+          <div className="bg-light px-1">Category 1</div>
+        </Col>
+        <Col xs={4} md={3} className="bg-secondary  p-2 d-flex align-items-end">
+          <div className="bg-light px-1">Category 2</div>
+        </Col>
+      </Row>
+      <Row
+        className="gap-2 d-flex justify-content-center py-1  text-uppercase  fw-light"
+        style={{ height: "200px", fontSize: "12px" }}
+      >
+        <Col xs={4} md={3} className="bg-secondary  p-2 d-flex align-items-end">
+          <div className="bg-light px-1">Category 3</div>
+        </Col>
+        <Col xs={7} md={8} className="bg-secondary  p-2 d-flex align-items-end">
+          <div className="bg-light px-1">Category 4</div>
+        </Col>
+      </Row>
+      <Row className="border border-2 border-start-0 border-end-0 my-4 py-2 text-center text-secondary">
+        <h1>Meu BR Alcobaça</h1>
+      </Row>
+    </Container>
+  );
 }
 
-export default App
+export default App;
