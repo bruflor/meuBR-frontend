89b687bcf8f62e8c00eb674d368f7d9d2077abec
Bruna Flor <brn.flor@gmail.com>
2022-11-03 18:01:48 +0000

    basic structure of Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import { Button, Col, Container, Row } from "react-bootstrap";
+import { Icon } from "@iconify/react";
+
 import "./App.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <Container className="App ">
-      <Row className="border border-2 border-start-0 border-end-0 border-secondary my-4 mx-2 p-3 text-center text-secondary">
+      <Row className="border border-2 border-start-0 border-end-0 border-secondary my-4 p-3 text-center text-secondary">
         <h1 className="fw-bold">Meu BR Alcobaça</h1>
       </Row>
-      <div className="d-flex bg-secondary bg-opacity-10 p-3 mb-5">
+      <div className="d-flex bg-secondary bg-opacity-10 p-3 mb-5 justify-content-between">
         search here
+        <Icon icon="akar-icons:search" />
       </div>
+      {/* categories */}
       <div>
         <Row
           className="gap-2 d-flex justify-content-center py-1 text-uppercase  fw-light"
@@ -19,39 +23,39 @@ function App() {
           <Col
             xs={7}
             md={8}
-            className="bg-secondary  p-2 d-flex align-items-end p-2"
+            className="bg-secondary p-2 d-flex align-items-end p-2"
           >
             <div className="bg-light px-1">Category 1</div>
           </Col>
           <Col
             xs={4}
             md={3}
-            className="bg-secondary  p-2 d-flex align-items-end"
+            className="bg-secondary p-2 d-flex align-items-end"
           >
             <div className="bg-light px-1">Category 2</div>
           </Col>
         </Row>
         <Row
-          className="gap-2 d-flex justify-content-center py-1  text-uppercase  fw-light"
+          className="gap-2 d-flex justify-content-center py-1 text-uppercase  fw-light"
           style={{ height: "200px", fontSize: "12px" }}
         >
           <Col
             xs={4}
             md={3}
-            className="bg-secondary  p-2 d-flex align-items-end"
+            className="bg-secondary  p-2 d-flex align-items-end p-2"
           >
             <div className="bg-light px-1">Category 3</div>
           </Col>
           <Col
             xs={7}
             md={8}
-            className="bg-secondary  p-2 d-flex align-items-end"
+            className="bg-secondary  p-2 d-flex align-items-end p-2"
           >
             <div className="bg-light px-1">Category 4</div>
           </Col>
         </Row>
       </div>
-      <div className=" d-flex flex-row-reverse my-1">
+      <div className=" d-flex flex-row-reverse my-2">
         <Button
           size="sm"
           className="bg-secondary bg-opacity-25 border-0 text-secondary"
@@ -59,8 +63,56 @@ function App() {
           Todas as categorias
         </Button>
       </div>
-      <Row className="border-2 border-bottom border-secondary m-2 mt-5 text-left text-secondary">
+      <Row className="border-2 border-bottom border-secondary my-4 text-left text-secondary">
         <h2 className="">News</h2>
+      </Row>
+      <Row>
+        <Col xs={12} md={6}>
+          <div className="bg-secondary " style={{ height: "400px" }}></div>
+          <span className="text-secondary">category of news</span>
+          <h3>Title of News</h3>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia
+            deserunt ad voluptatibus earum sed qui facere sapiente. Magnam eius
+            maxime, cum fugiat dignissimos quaerat voluptates nesciunt cumque?
+            Incidunt, consequuntur itaque?
+          </p>
+        </Col>
+        <Col xs={12} md={6} className="d-flex flex-column gap-3">
+          <Col className="d-flex gap-3">
+            <div className="bg-secondary" style={{ minWidth: "180px" }}></div>
+            <div>
+              <span className="text-secondary">category of news</span>
+              <h3>Title of News</h3>
+              <p className="m-0">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Mollitia deserunt ad voluptatibus earum sed qui facere sapiente.
+              </p>
+            </div>
+          </Col>
+          <Col className="d-flex gap-3">
+            <div className="bg-secondary" style={{ minWidth: "180px" }}></div>
+            <div>
+              <span className="text-secondary">category of news</span>
+              <h3>Title of News</h3>
+              <p className="m-0">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Mollitia deserunt ad voluptatibus earum sed qui facere sapiente.
+              </p>
+            </div>
+          </Col>
+          <Col className="d-flex gap-3">
+            <div className="bg-secondary" style={{ minWidth: "180px" }}></div>
+            <div>
+              <span className="text-secondary">category of news</span>
+              <h3>Title of News</h3>
+              <p className="m-0">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Mollitia deserunt ad voluptatibus earum sed qui facere sapiente.
+              </p>
+            </div>
+          </Col>
+        </Col>
       </Row>
     </Container>
   );
